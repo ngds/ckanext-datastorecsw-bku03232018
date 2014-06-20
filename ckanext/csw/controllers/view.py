2d124067e55515fc4f29d2cbeb3cac3bb3bc7b29
asonnenschein <adrian.sonnenschein@gmail.com>
2014-06-20 14:25:45 -0700

    serialize json from python dict

--- ckanext/csw/controllers/view.py
+++ ckanext/csw/controllers/view.py
@@ -1,20 +1,42 @@
+import json
+import xml.etree.ElementTree as etree
+try:
+    xml_parser_exception = etree.ParseError
+except AttributeError:
+    from xml.parsers import expat
+    xml_parser_exception = expat.ExpatError
 import ckan.plugins as p
 from ckan import model
-from ckan.lib.base import BaseController, c, abort
+from ckan.lib.base import BaseController, c, abort, response
 from pylons.i18n import _
 
 class ViewController(BaseController):
-    def show_object(self, id, ref_type='object'):
+    def show_object(self, id):
         try:
             context = {'model': model, 'user': c.user}
-            if ref_type == 'object':
-                obj = p.toolkit.get_action('datastore_package_show')(context, {'id': id})
-            elif ref_type == 'dataset':
-                obj = p.toolkit.get_action('datastore_package_show')(context, {'dataset_id': id})
+            obj = p.toolkit.get_action('datastore_package_show')(context, {'id': id})
+
+            try:
+                if obj['result']:
+                    content = obj['result']
+                else:
+                    abort(404, _('No content found'))
+
+#                try:
+#
+ #               except:
+#
+            except xml_parser_exception:
+                try:
+                    pkg = json.dumps(content)
+                    response.content_type = 'application/json; charset=utf-8'
+                    response.headers['Content-Length'] = len(obj)
+                    return pkg.encode('utf-8')
+
         except p.toolkit.ObjectNotFound, e:
-            abort(404,_(str(e)))
+            abort(404, _(str(e)))
         except p.toolkit.NotAuthorized:
             abort(401, self.not_auth_message)
         except Exception, e:
             msg = 'An error ocurred: [%s]' % str(e)
-            abort(500g)
+            abort(500)
--- ckanext/csw/logic/action.py
+++ ckanext/csw/logic/action.py
@@ -1,10 +1,17 @@
 from ckan import logic
+import ckan.plugins as p
 from ckan.logic import side_effect_free
 
 @side_effect_free
 def datastore_package_show(context, data_dict):
-    id = data_dict['id']
-    dataset_id = data_dict[dataset_id]
 
-    print data_dict
-    return
+    id = data_dict['id']
+
+    if id:
+        pkg = logic.action.get.package_show(context, data_dict)
+        if not pkg:
+            raise p.toolkit.ObjectNotFound('Dataset not found')
+    else:
+        raise p.toolkit.ValidationError('Please provide a package ID')
+
+    return pkg
